1746d2a861af374d37c3ecda3c6cf208c03a817d
Jacobo Monroy <162770749+KillahBlitz@users.noreply.github.com>
2025-10-04 03:39:04 -0600

    primer endpoint listo

--- backend/methods/crear_tarea.py
+++ backend/methods/crear_tarea.py
@@ -2,16 +2,20 @@ from models.Tarea import Tarea
 
 tareas = []
 
-def crear(tarea:Tarea):
+def crear(tarea:dict):
     try:
-        nueva_tarea = tarea.dict()
-        nueva_tarea["id"] = len(tareas) + 1
-        guardar_tarea(nueva_tarea)
-        return {"mensaje": "Tarea creada", "tarea": nueva_tarea}
+        tarea["id"] = len(tareas) + 1
+        tarea = Tarea(**tarea)
+        if guardar_tarea(tarea):
+            return {"status": "success", "tarea": tarea}
+        else:
+            return {"status": "Error", "menssage": "No se pudo guardar la tarea"}
     except Exception as e:
-        return {"status": "Error", "menssage": e}
+        return {"status": "Error", "menssage": "No se mandaron los datos esperados"}
 
 def guardar_tarea(tarea):
-
-    tareas.append(tarea)
-    return True
+    try:
+        tareas.append(tarea)
+        return True
+    except Exception as e:
+        return False
