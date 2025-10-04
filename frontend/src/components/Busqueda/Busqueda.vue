f6b2aadbffd7c4f3085c1c0a374f7ace3280aa08
Jacobo Monroy <162770749+KillahBlitz@users.noreply.github.com>
2025-10-04 03:24:27 -0600

    agregue componente de barra de creacion con descripcion

--- frontend/src/components/Busqueda/Busqueda.vue
+++ frontend/src/components/Busqueda/Busqueda.vue
@@ -1,9 +1,22 @@
 <script setup>
+import './Busqueda.css';
+import fetchData from './Busqueda.js';
+
+const handleClick = () => {
+  // recuperar los valores de los inputs
+  const task = document.querySelector('.Task').value;
+  const description = document.querySelector('.descripcion').value;
+  fetchData(task, description);
+  document.querySelector('.Task').value = '';
+  document.querySelector('.descripcion').value = '';
+};
 </script>
 
 <template>
-  <div>
-    <h1>Helloworld</h1>
+  <div class="search-bar">
+    <input class="Task" type="text" placeholder="Crear tarea..." />
+    <input class="descripcion" type="text" placeholder="Descripcion..." />
+    <button class="boton" @click="handleClick">Buscar</button>
   </div>
 </template>
 
