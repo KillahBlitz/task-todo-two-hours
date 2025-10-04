7aa350400b40f2d00332dae492b72cde74b9aa7d
Jacobo Monroy <162770749+KillahBlitz@users.noreply.github.com>
2025-10-04 02:36:52 -0600

    integracion de los dos primeros componentes

---
+++ frontend/src/components/Header/Header.vue
@@ -0,0 +1,14 @@
+<script setup>
+import './Header.css';
+</script>
+
+<template>
+  <div>
+    <fa :icon="['fas', 'list-check']" style="font-size: 100px; color: #42b983; margin-right: 10px;" />
+    <h1>Task Todo</h1>
+    <h2>Ingresa tu Tarea por Hacer</h2>
+  </div>
+</template>
+
+<style scoped>
+</style>
